9e6912df4c5335c5e961549b77fa15d7ce9d5c09
Newman <newman.kcchow@gmail.com>
2022-07-02 23:40:24 +0800

    Setup storybook and astro projects

---
+++ packages/ui/src/components/Button.vue
@@ -0,0 +1,3 @@
+<template>
+<button>AB</button>
+</template>
---
+++ packages/ui/vite.config.ts
@@ -0,0 +1,28 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), dts()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/lib.ts'),
+      name: 'UI',
+      fileName: (format) => `index.${format}.js`
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
+})
