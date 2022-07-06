7956ee398205957088afcd53dc7d632e02992dd3
inizio <newman.kcchow@gmail.com>
2022-07-07 00:35:25 +0800

    Simple test case for SkipTo

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -10,14 +10,16 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
   ],
   "framework": "@storybook/vue3",
   "core": {
     "builder": "@storybook/builder-vite"
   },
   "features": {
-    "storyStoreV7": true
+    "storyStoreV7": true,
+    interactionsDebugger: true,
   },
   async viteFinal(config, { configType }) {
     // return the customized config
--- packages/ui/src/stories/SkipLink.stories.js
+++ packages/ui/src/stories/SkipLink.stories.js
@@ -1,4 +1,8 @@
+import { userEvent, within } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+
 import { SkipTo } from '../lib';
+import { useEventSource } from '@vueuse/core';
 
 // More on default export: https://storybook.js.org/docs/vue/writing-stories/introduction#default-export
 export default {
@@ -17,9 +21,27 @@ const Template = (args) => ({
     return { args };
   },
   // And then the `args` are bound to your component with `v-bind="args"`
-  template: '<SkipTo to="#target" /><div style="height: 1000px"></div><div id="target" style="height: 50px; width: 50px; background-color: red"></div>',
+  template: '<SkipTo to="#target" /><div data-testid="empty-space" style="height: 10000px"></div><div data-testid="target" style="height: 50px; width: 50px; background-color: red"></div>',
 });
 
 export const Default = Template.bind({});
 // More on args: https://storybook.js.org/docs/vue/writing-stories/args
 Default.args = {};
+Default.play = ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+
+  expect(
+    canvas.getByText(
+      'Skip to main content'
+    )
+  ).not.toBeVisible();
+
+  userEvent.click(canvas.getByTestId('empty-space'))
+  userEvent.tab();
+
+  expect(
+    canvas.getByText(
+      'Skip to main content'
+    )
+  ).toBeVisible();
+};
